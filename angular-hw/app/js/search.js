e5777690c0d81776d3c154c7ad9567e150f653f4
zeroetun <shivan_4@hotmail.com>
2014-04-19 17:28:21 +0200

    angular init

---
+++ angular-hw/app/js/search.js
@@ -0,0 +1,19 @@
+function SearchCtrl($scope, $http) {
+    $scope.url = 'search.php'; // The url of our search
+         
+    // The function that will be executed on button click (ng-click="search()")
+    $scope.search = function() {
+         
+        // Create the http post request
+        // the data holds the keywords
+        // The request is a JSON request.
+        $http.post($scope.url, { "data" : "hello world"}).
+        success(function(data, status) {
+            $scope.result = data; // Show result from server in our <pre></pre> element
+        })
+        .
+        error(function(data, status) {
+            $scope.result = data || "Request failed";            
+        });
+    };
+}
