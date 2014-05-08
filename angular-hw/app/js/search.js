fba637b01657d201c75ebb49b3f79c0de1041092
mpouedras <mpouedras@gmail.com>
2014-05-08 16:06:11 +0200

    node now serve static file

--- angular-hw/app/js/search.js
+++ angular-hw/app/js/search.js
@@ -1,5 +1,5 @@
 function SearchCtrl($scope, $http) {
-    $scope.url = 'search.php'; // The url of our search
+    $scope.url = 'http://localhost:8080/api/visitors'; // The url of our search
          
     // The function that will be executed on button click (ng-click="search()")
     $scope.search = function() {
@@ -7,7 +7,7 @@ function SearchCtrl($scope, $http) {
         // Create the http post request
         // the data holds the keywords
         // The request is a JSON request.
-        $http.post($scope.url, { "data" : "hello world"}).
+        $http.get($scope.url, { "data" : "hello world"}).
         success(function(data, status) {
             $scope.result = data; // Show result from server in our <pre></pre> element
         })
