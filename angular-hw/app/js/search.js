6c51a7ea23b60d17570e7cb784582ed846dac862
zeroetun <shivan_4@hotmail.com>
2014-05-08 16:08:42 +0200

    some changes

--- angular-hw/app/js/search.js
+++ angular-hw/app/js/search.js
@@ -1,5 +1,5 @@
 function SearchCtrl($scope, $http) {
-    $scope.url = 'search.php'; // The url of our search
+    $scope.url = 'http://localhost:8080/api/visitors'; // The url of our search
          
     // The function that will be executed on button click (ng-click="search()")
     $scope.search = function() {
@@ -7,12 +7,14 @@ function SearchCtrl($scope, $http) {
         // Create the http post request
         // the data holds the keywords
         // The request is a JSON request.
-        $http.post($scope.url, { "data" : "hello world"}).
+        $http({ method: 'GET', url: 'http://localhost:8080/api/visitors' }).
         success(function(data, status) {
+            console.log("test" + data)
             $scope.result = data; // Show result from server in our <pre></pre> element
         })
         .
         error(function(data, status) {
+            console.log("error" + data)
             $scope.result = data || "Request failed";            
         });
     };
