c89c524a37b7448b8323e14c6d3df288f6dff9db
mpouedras <mpouedras@gmail.com>
2014-05-08 18:13:08 +0200

    manual registration implementation

--- angular-hw/app/js/search.js
+++ angular-hw/app/js/search.js
@@ -1,5 +1,5 @@
 function SearchCtrl($scope, $http) {
-    $scope.url = 'http://localhost:8080/api/visitors'; // The url of our search
+    $scope.url = 'http://localhost:8080/api/'; // The url of our search
          
     // The function that will be executed on button click (ng-click="search()")
     $scope.search = function() {
@@ -16,4 +16,16 @@ function SearchCtrl($scope, $http) {
             $scope.result = data || "Request failed";            
         });
     };
+
+    $scope.iAmHere = function() {
+        var data = {country: $scope.country, city: $scope.city}
+        $http.post($scope.url + 'visitor', data)
+            .success(function(data, status) {
+                $scope.visitorId = data._id;
+                
+            })
+            .error(function(data, status) {
+            $scope.result = data || "Request failed";            
+        });
+    }
 }
