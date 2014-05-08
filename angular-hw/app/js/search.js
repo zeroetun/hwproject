b2906306aa34e95a9b7cbc0271d17fd212474f4a
zeroetun <shivan_4@hotmail.com>
2014-05-08 18:21:39 +0200

    delete client and display

--- angular-hw/app/js/search.js
+++ angular-hw/app/js/search.js
@@ -1,5 +1,6 @@
 function SearchCtrl($scope, $http) {
-    $scope.url = 'http://localhost:8080/api/visitors'; // The url of our search
+    $scope.url = 'http://localhost:8080/api/'; // The url of our search
+    $scope.id = "536b9edeac1031d013000001";
          
     // The function that will be executed on button click (ng-click="search()")
     $scope.search = function() {
@@ -7,7 +8,7 @@ function SearchCtrl($scope, $http) {
         // Create the http post request
         // the data holds the keywords
         // The request is a JSON request.
-        $http.get($scope.url).
+        $http.get($scope.url+"/visitors").
         success(function(data, status) {
             console.log(data);
             $scope.result = data; // Show result from server in our <pre></pre> element
@@ -16,4 +17,14 @@ function SearchCtrl($scope, $http) {
             $scope.result = data || "Request failed";            
         });
     };
+
+    $scope.delete = function() {
+        $http.delete($scope.url+"/visitor/"+$scope.id).
+        success(function(data, status) {
+             $scope.result = "success"; // Show result from server in our <pre></pre> element
+        }).
+        error(function(data, status) {
+            $scope.result = data || "Delete failed";            
+        });
+    }
 }
