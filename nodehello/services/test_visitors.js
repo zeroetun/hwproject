cd15fd94a016e823148598bb7532a2ad5601006c
mpouedras <mpouedras@gmail.com>
2014-07-20 17:34:23 +0200

    callback added to servies. First micha test

--- nodehello/services/test_visitors.js
+++ nodehello/services/test_visitors.js
@@ -1,6 +1,51 @@
 var visitors = require('./visitors');
+var mongoose = require('mongoose');
+var Visitor = require('../models/visitor');
 
-// TESTS
-//visitors.register("2", "3");
-visitors.remove("53cbcd29ca08a9df16000001");
-//visitors.findAll();
+mongoose.connect('mongodb://127.0.0.1:27017/hello');
+
+Visitor.remove({}, function(err, nRemoved) {
+	if (err) throw err;
+	console.log("Drop de la collection, eléments supprimés : ", nRemoved);
+});
+
+var newVisitorId;
+var assert = require("assert")
+describe('visitors', function(){
+  	describe('#register()', function(){
+    	it('should save without error', function(done){
+      		visitors.register("longitude", "latitude", function(err, visitorId) {
+				if (err) throw err;
+				newVisitorId = visitorId;
+				done();
+			})
+		});
+    })
+  })
+
+var assert = require("assert")
+describe('visitors', function(){
+  	describe('#findAll()', function(){
+    	it('should list 1 visitor', function(done){
+      		visitors.findAll(function(err, visitors) {
+				if (err) throw err;
+				assert.equal(1, visitors.length);
+				done();
+			})
+		});
+    })
+  })
+
+var assert = require("assert")
+describe('visitors', function(){
+  	describe('#remove()', function(){
+    	it('should remove specified visitor', function(done){
+      		visitors.remove(newVisitorId, function(err, nRemoved) {
+				if (err) throw err;
+				assert.equal(1, nRemoved);
+				done();
+			})
+		});
+    })
+  })
+
--- nodehello/services/visitors.js
+++ nodehello/services/visitors.js
@@ -1,30 +1,30 @@
 var Visitor = require('../models/visitor');
 
-var register = function(longitude, latitude) {
+var register = function(longitude, latitude, callback) {
 	var visitor = new Visitor();
 	visitor.longitude = longitude;
 	visitor.latitude = latitude;
 	visitor.date = new Date();
 	
-	visitor.save();
-}
-
-var remove = function(id) {
-	Visitor.remove({_id: id}, function(err, nRemoved) {
-		if (err) console.log(err);
-		console.log(nRemoved);
+	visitor.save(function(err, visitorId) {
+		callback(err, visitor._id);
 	});
 }
 
-var findAll = function() {
+var remove = function(id, callback) {
+	Visitor.remove({_id: id}, function(err, nRemoved) {
+		callback(err, nRemoved);
+	});
+}
+
+var findAll = function(callback) {
 	Visitor.find(function(err, visitors) {
-    	if (err) console.log(err);
-    	console.log(visitors);
+    	callback(err, visitors);
     });
 }
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/hello'); // connect to our database
+
+
 
 module.exports.register = register;
 module.exports.remove = remove;
